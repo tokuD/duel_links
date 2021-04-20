745ddd3f4faf66e4ce6f69e4067338f7571fbe90
tokuD <megumu112851@gmail.com>
2021-04-20 23:45:56 +0900

    first commit

--- league_site/league/urls.py
+++ league_site/league/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'league'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', auth_views.LoginView.as_view(template_name='league/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('users/<int:pk>', views.users_detail, name='users_detail'),
 ]
--- league_site/league/views.py
+++ league_site/league/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 def index(request):
-    return render(request, 'league/index.html', {})
+    return render(request, 'league/index.html')
+
+def users_detail(request, pk):
+    return render(request, 'league/users_detail.html')
